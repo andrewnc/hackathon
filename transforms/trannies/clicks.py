72d2f83041814b63974f19acbdee9e78a66107f1
Andrew Carr <andrewcarr@Andrews-MBP.app.byu.edu>
2016-11-18 11:52:38 -0700

    sweet points click

---
+++ transforms/trannies/clicks.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(-10,10)
+y = 0*x
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(x,y)
+
+coords = []
+
+def x_of_t(p1,p2,p3,p4,t):
+    return p1[0]*(1-t)**3 + 3*p2[0]*(1-t)**2*t+3*p3[0]*(1-t)*(t**2)+p4[0]*t**3
+
+def y_of_t(p1,p2,p3,p4,t):
+    return p1[1]*(1-t)**3 + 3*p2[1]*(1-t)**2*t+3*p3[1]*(1-t)*(t**2)+p4[1]*t**3
+
+def onclick(event):
+    global ix, iy
+    ix, iy = event.xdata, event.ydata
+    print 'x = %d, y = %d'%(
+        ix, iy)
+
+    global coords
+    coords.append((ix, iy))
+
+    if len(coords) == 4:
+        fig.canvas.mpl_disconnect(cid)
+        plt.close()
+
+    return coords
+
+
+cid = fig.canvas.mpl_connect('button_press_event', onclick)
+plt.show()
+
+
+t = np.linspace(0,1,1000)
+plt.plot(x_of_t(coords[0],coords[1],coords[2],coords[3],t),y_of_t(coords[0],coords[1],coords[2],coords[3],t))
+plt.show()
