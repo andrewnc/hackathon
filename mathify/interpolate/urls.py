6f562db9378a061f720b312b936bddb9c5d02202
Andrew Carr <andrewcarr@Andrews-MBP-010024031208.app.byu.edu>
2016-11-19 18:05:10 -0700

    fix url structure

--- mathify/interpolate/urls.py
+++ mathify/interpolate/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'landing/', views.landing_render, name='landing'),
+    url(r'^$', views.landing_render, name='landing'),
+    url(r'submit/', views.index, name='index'),
     url(r'info/', views.getFormData, name='info'),
 
 ]
